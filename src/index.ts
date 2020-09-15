a3addfeaa43f1aca52f70f980d2cbaa8ec1eada0
Joo Park <jude.park@linecorp.com>
2020-09-15 18:08:47 +0900

    Add core module

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,55 @@
+import CoreAssistant from '@sketch-hq/sketch-core-assistant'
 import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 
-const helloWorld: RuleDefinition = {
+const textDisallow: RuleDefinition = {
   rule: async (context) => {
-    context.utils.report('Hello world')
+    const { utils } = context
+
+    // Get a configuration option named "pattern"
+    const pattern = utils.getOption('pattern')
+    if (typeof pattern !== 'string') throw Error()
+
+    // Iterate
+    for (const layer of utils.objects.text) {
+      const value = layer.attributedString.string
+      // Test
+      if (value.includes(pattern)) {
+        // Report
+        utils.report(`Layer “${layer.name}” contains “${pattern}”`, layer)
+      }
+    }
   },
-  name: 'sketch-assistant-template/hello-world',
-  title: 'Hello World',
-  description: 'Reports a hello world message',
+  name: 'ldsg-assistant/text-disallow',
+  title: (config) => `Text should not contain "${config.pattern}"`,
+  description: 'Reports a violation when text layers contain a configurable text pattern',
 }
 
-const assistant: AssistantPackage = async () => {
-  return {
-    name: 'sketch-assistant-template',
-    rules: [helloWorld],
-    config: {
-      rules: {
-        'sketch-assistant-template/hello-world': { active: true },
+const assistant: AssistantPackage = [
+  CoreAssistant,
+  async () => {
+    return {
+      name: 'ldsg-assistant',
+      rules: [textDisallow],
+      config: {
+        rules: {
+          '@sketch-hq/sketch-core-assistant/symbols-no-unused': { active: true },
+          '@sketch-hq/sketch-core-assistant/symbols-no-detached': { active: true },
+          '@sketch-hq/sketch-core-assistant/layer-styles-prefer-shared': {
+            active: false,
+            maxIdentical: 2,
+          },
+          '@sketch-hq/sketch-core-assistant/text-styles-prefer-shared': {
+            active: false,
+            maxIdentical: 2,
+          },
+          'ldsg-assistant/text-disallow': {
+            pattern: 'Type something',
+            active: false,
+          },
+        },
       },
-    },
-  }
-}
+    }
+  },
+]
 
 export default assistant
